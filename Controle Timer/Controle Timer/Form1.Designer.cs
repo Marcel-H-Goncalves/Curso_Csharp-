0e8ed0a84bdbb5f4ceb7a90fb8be448d06b1866b
Marcel Henrique Gonçalves <marcel.henrique1989@gmail.com>
2025-01-07 05:12:18 -0300

    Atualização da Aula Timer
    como criar e usar o timer no forms

--- Controle Timer/Controle Timer/Form1.Designer.cs
+++ Controle Timer/Controle Timer/Form1.Designer.cs
@@ -28,12 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            timerInterfaceGrafica = new System.Windows.Forms.Timer(components);
+            buttonHabilitarTimerInterfaceGrafica = new Button();
+            buttonDesabilitarTimerInterfaceGrafica = new Button();
+            buttonHabilitarTimerPorCodigo = new Button();
+            buttonDesabilitarTimerPorCodigo = new Button();
+            SuspendLayout();
+            // 
+            // timerInterfaceGrafica
+            // 
+            timerInterfaceGrafica.Interval = 5000;
+            timerInterfaceGrafica.Tick += timerInterfaceGrafica_Tick;
+            // 
+            // buttonHabilitarTimerInterfaceGrafica
+            // 
+            buttonHabilitarTimerInterfaceGrafica.Location = new Point(35, 81);
+            buttonHabilitarTimerInterfaceGrafica.Name = "buttonHabilitarTimerInterfaceGrafica";
+            buttonHabilitarTimerInterfaceGrafica.Size = new Size(427, 39);
+            buttonHabilitarTimerInterfaceGrafica.TabIndex = 0;
+            buttonHabilitarTimerInterfaceGrafica.Text = "Habilitar Timer Interface Gráfica";
+            buttonHabilitarTimerInterfaceGrafica.UseVisualStyleBackColor = true;
+            buttonHabilitarTimerInterfaceGrafica.Click += buttonHabilitaTimerInterfaceGrafica_Click;
+            // 
+            // buttonDesabilitarTimerInterfaceGrafica
+            // 
+            buttonDesabilitarTimerInterfaceGrafica.Location = new Point(35, 126);
+            buttonDesabilitarTimerInterfaceGrafica.Name = "buttonDesabilitarTimerInterfaceGrafica";
+            buttonDesabilitarTimerInterfaceGrafica.Size = new Size(427, 39);
+            buttonDesabilitarTimerInterfaceGrafica.TabIndex = 1;
+            buttonDesabilitarTimerInterfaceGrafica.Text = "Desabilitar Timer Interface Gráfica";
+            buttonDesabilitarTimerInterfaceGrafica.UseVisualStyleBackColor = true;
+            buttonDesabilitarTimerInterfaceGrafica.Click += button2_Click;
+            // 
+            // buttonHabilitarTimerPorCodigo
+            // 
+            buttonHabilitarTimerPorCodigo.Location = new Point(35, 201);
+            buttonHabilitarTimerPorCodigo.Name = "buttonHabilitarTimerPorCodigo";
+            buttonHabilitarTimerPorCodigo.Size = new Size(167, 59);
+            buttonHabilitarTimerPorCodigo.TabIndex = 2;
+            buttonHabilitarTimerPorCodigo.Text = "Habilitar Timer Por Código";
+            buttonHabilitarTimerPorCodigo.UseVisualStyleBackColor = true;
+            buttonHabilitarTimerPorCodigo.Click += buttonHabilitarTimerPorCodigo_Click;
+            // 
+            // buttonDesabilitarTimerPorCodigo
+            // 
+            buttonDesabilitarTimerPorCodigo.Location = new Point(295, 201);
+            buttonDesabilitarTimerPorCodigo.Name = "buttonDesabilitarTimerPorCodigo";
+            buttonDesabilitarTimerPorCodigo.Size = new Size(167, 59);
+            buttonDesabilitarTimerPorCodigo.TabIndex = 3;
+            buttonDesabilitarTimerPorCodigo.Text = "Desabilitar Timer Por Código";
+            buttonDesabilitarTimerPorCodigo.UseVisualStyleBackColor = true;
+            buttonDesabilitarTimerPorCodigo.Click += buttonDesabilitarTimerPorCodigo_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(buttonDesabilitarTimerPorCodigo);
+            Controls.Add(buttonHabilitarTimerPorCodigo);
+            Controls.Add(buttonDesabilitarTimerInterfaceGrafica);
+            Controls.Add(buttonHabilitarTimerInterfaceGrafica);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer timerInterfaceGrafica;
+        private Button buttonHabilitarTimerInterfaceGrafica;
+        private Button buttonDesabilitarTimerInterfaceGrafica;
+        private Button buttonHabilitarTimerPorCodigo;
+        private Button buttonDesabilitarTimerPorCodigo;
     }
 }
--- Controle Timer/Controle Timer/Form1.cs
+++ Controle Timer/Controle Timer/Form1.cs
@@ -2,9 +2,44 @@ namespace Controle_Timer
 {
     public partial class Form1 : Form
     {
+        System.Windows.Forms.Timer timerPorCodigo;
         public Form1()
         {
             InitializeComponent();
+            timerPorCodigo = new System.Windows.Forms.Timer();
+            timerPorCodigo.Enabled = false;
+            timerPorCodigo.Interval = 5000;
+            timerPorCodigo.Tick += TimerPorCodigo_Tick;
+        }
+        private void TimerPorCodigo_Tick(Object? sender, EventArgs e)
+        {
+            MessageBox.Show("Timer por Código: " + DateTime.Now.ToString("hh:mm:ss"));
+        }
+
+        private void buttonHabilitaTimerInterfaceGrafica_Click(object sender, EventArgs e)
+        {
+            timerInterfaceGrafica.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timerInterfaceGrafica.Enabled = false;
+        }
+
+        private void timerInterfaceGrafica_Tick(object sender, EventArgs e)
+        {
+            MessageBox.Show("Timer interface gráfica: " + DateTime.Now.ToString("hh:mm:ss"));
+        }
+
+        private void buttonHabilitarTimerPorCodigo_Click(object sender, EventArgs e)
+        {
+            timerPorCodigo.Enabled = true;
+
+        }
+
+        private void buttonDesabilitarTimerPorCodigo_Click(object sender, EventArgs e)
+        {
+            timerPorCodigo.Enabled = false;
         }
     }
 }
