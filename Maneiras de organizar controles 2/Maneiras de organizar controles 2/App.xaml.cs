bd7377829331863074af8307896f360941dbe2b8
Marcel Henrique Gonçalves <marcel.henrique1989@gmail.com>
2025-01-16 05:23:38 -0300

    Organizando e criando Controles 2
    Nessa aula foi ensinado como criar controles e organiza-los usando Grid pela caixa de ferramentas

---
+++ Maneiras de organizar controles 2/Maneiras de organizar controles 2/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Maneiras_de_organizar_controles_2
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
