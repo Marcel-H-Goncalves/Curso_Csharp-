c238b69819508970388d5d433d192888c496cc94
Marcel Henrique Gonçalves <marcel.henrique1989@gmail.com>
2025-01-21 04:50:38 -0300

    Aula textBlock e Label
    É ensinado como usar textblock e Label no WPF

---
+++ TextBlock e Label/TextBlock e Label/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TextBlock_e_Label
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void label_MouseEnter(object sender, MouseEventArgs e)
+        {
+            label.FontSize = 20;
+            label.Background = new SolidColorBrush(Colors.Red);
+        }
+
+        private void label_MouseLeave(object sender, MouseEventArgs e)
+        {
+            label.FontSize = 15;
+            label.Background = new SolidColorBrush(Colors.White);
+        }
+    }
+}
