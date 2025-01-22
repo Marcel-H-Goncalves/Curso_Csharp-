bcb4e8b4c40930d0dfe214c2a2e57d3d1bd41945
Marcel Henrique Gonçalves <marcel.henrique1989@gmail.com>
2025-01-22 05:12:55 -0300

    Aula sobre TextBox e RichTextBox emWPF
    É ensinado sobre como usar e configurar textBox e RichTextBox no WPF e seus eventos

---
+++ TextBox e RichTextBox WPF/TextBox e RichTextBox WPF/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace TextBox_e_RichTextBox_WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ TextBox e RichTextBox WPF/TextBox e RichTextBox WPF/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TextBox_e_RichTextBox_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            TextBox1.Background = new SolidColorBrush(Colors.Red);
+        }
+
+        private void TextBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            TextBox1.Background = new SolidColorBrush(Colors.White);
+            MessageBox.Show("A tecla pressionada foi:" + e.Key.ToString());
+            e.Key.ToString();
+
+        }
+    }
+}
