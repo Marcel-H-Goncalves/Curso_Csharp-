0cf7a4ae446a5dc4c254792a212d5f06cd85dc81
Marcel Henrique Gonçalves <marcel.henrique1989@gmail.com>
2025-01-23 05:11:00 -0300

    Aula PasswordBox
    Ensinando como usar o PasswordBox e seus atributos

---
+++ Controle PasswordBox/Controle PasswordBox/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Controle_PasswordBox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void PasswordBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show(passwordBox.Password, "Aviso", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            passwordBox.Visibility = Visibility.Collapsed;
+            textBox.Visibility = Visibility.Visible;
+            textBox.Text = passwordBox.Password;
+        }
+
+        private void Button_MouseLeave(object sender, MouseEventArgs e)
+        {
+            passwordBox.Visibility = Visibility.Visible;
+            textBox.Visibility = Visibility.Collapsed;
+            textBox.Text ="";
+        }
+    }
+}
