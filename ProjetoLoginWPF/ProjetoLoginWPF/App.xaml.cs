4c0108cee12663ef8417282815383936f5e219a3
Marcel Henrique Gonçalves <marcel.henrique1989@gmail.com>
2025-01-23 05:28:38 -0300

    Projeto Login
    Criando um projeto de Login usando WPF e o que foi aprendido nas ultimas aulas

---
+++ ProjetoLoginWPF/ProjetoLoginWPF/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ProjetoLoginWPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
