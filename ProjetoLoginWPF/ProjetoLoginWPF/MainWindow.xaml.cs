b58a955bb15e6133bce742b11519d43c4c69acde
Marcel Henrique Gonçalves <marcel.henrique1989@gmail.com>
2025-01-24 05:22:26 -0300

    ProjetoLoginWPF
    criação da lógica do programa com adição de nova janela

--- ProjetoLoginWPF/ProjetoLoginWPF/MainWindow.xaml.cs
+++ ProjetoLoginWPF/ProjetoLoginWPF/MainWindow.xaml.cs
@@ -20,5 +20,54 @@ namespace ProjetoLoginWPF
         {
             InitializeComponent();
         }
+
+        private void buttonShow_Click(object sender, RoutedEventArgs e)
+        {
+            passwordBoxSenha.Visibility = Visibility.Collapsed;
+            textBoxSenha.Visibility = Visibility.Visible;
+            textBoxSenha.Text = passwordBoxSenha.Password;
+        }
+
+        private void buttonShow_MouseLeave(object sender, MouseEventArgs e)
+        {
+            textBoxSenha.Visibility = Visibility.Collapsed;
+            passwordBoxSenha.Visibility = Visibility.Visible;
+            textBoxSenha.Text = "";
+        }
+
+        private void buttonEntrar_Click(object sender, RoutedEventArgs e)
+        {
+            if (buttonEntrar.Content.ToString() == "Entrar")
+            {
+                if (textBoxUsuario.Text == "Marcel")
+                {
+                    if (passwordBoxSenha.Password == "1234")
+                    {
+                        //Usuário logado no sistema
+                        MessageBox.Show("Usuário logado com sucesso", "Sucesso", MessageBoxButton.OK, MessageBoxImage.Information);
+                        buttonEntrar.Content = "Sair";
+                        buttonEntrar.Background = new SolidColorBrush(Colors.Green);
+                        passwordBoxSenha.Password = "";
+                        MeuPrograma meuPrograma = new MeuPrograma();
+                        meuPrograma.Show();
+                    }
+                    else
+                    {
+                        //Senha está incorreta
+                        MessageBox.Show("Senha está incorreta!", "Erro", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+                else
+                {
+                    //Usuário incorreto
+                    MessageBox.Show("Usuário está incorreto!", "Erro", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            else
+            {
+                buttonEntrar.Content = "Entrar";
+                buttonEntrar.Background = new SolidColorBrush(Color.FromArgb(221, 221, 221, 221));
+            }
+        }
     }
 }
