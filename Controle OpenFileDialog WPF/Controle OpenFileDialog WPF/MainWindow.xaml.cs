7f9ab4d0d5995ee8104684812ddde267b97f4d42
Marcel Henrique Gonçalves <marcel.henrique1989@gmail.com>
2025-01-31 05:43:55 -0300

    Aula Controle OpenFileDialog WPF
    É ensinado a usar o controle OpenFileDialog , como usar filtros de arquivos e capturar o caminho do arquivo

---
+++ Controle OpenFileDialog WPF/Controle OpenFileDialog WPF/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace Controle_OpenFileDialog_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog diag = new OpenFileDialog();
+            diag.InitialDirectory = @"c:\";
+            diag.Filter = "Todos os arquivos |*.*|Arquivos txt|*.txt| Arquivos pdf|*.pdf";
+            if(diag.ShowDialog() == true)
+            {
+                string caminhoArquivo = diag.FileName;
+                MessageBox.Show(caminhoArquivo);
+            }
+        }
+    }
+}
