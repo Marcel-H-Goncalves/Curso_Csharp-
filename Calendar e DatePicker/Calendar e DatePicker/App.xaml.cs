e6442e678f2df18df54b03fc20e399094d566a9a
Marcel Henrique Gonçalves <marcel.henrique1989@gmail.com>
2025-01-31 05:25:34 -0300

    Aula Calendar e DatePicker
    É ensinado a usar os controles e diferenças entre Calendar e DatePicker e suas propriedades e eventos

---
+++ Calendar e DatePicker/Calendar e DatePicker/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Calendar_e_DatePicker
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Calendar e DatePicker/Calendar e DatePicker/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calendar_e_DatePicker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void calendar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        {
+            SelectedDatesCollection todasAsDatas = calendar.SelectedDates;
+            foreach(DateTime data in todasAsDatas)
+            {
+                MessageBox.Show(data.ToString("dd/MM/yyyy"), "Aviso", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+        private void datePicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            label.Content = datePicker.SelectedDate.ToString();
+        }
+    }
+}
