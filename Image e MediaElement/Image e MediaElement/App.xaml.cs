7394e166726447657811097a0732801e954b26cf
Marcel Henrique Gonçalves <marcel.henrique1989@gmail.com>
2025-02-03 05:01:27 -0300

    Aula Image e MediaElement
    É aprendido a usar os controles image e mediaElement como colocar imagens e videos e seus parametros

---
+++ Image e MediaElement/Image e MediaElement/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Image_e_MediaElement
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Image e MediaElement/Image e MediaElement/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Image_e_MediaElement
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Uri enderecoImagem = new Uri(@"C:\Users\marce\source\repos\Image e MediaElement\Need for Speed™ Unbound 26_11_2023 14_41_57.png");
+            BitmapImage imagem = new BitmapImage(enderecoImagem);
+            image.Source = imagem;
+
+            
+            Uri enderecoVideo = new Uri(@"C:\Users\marce\source\repos\Image e MediaElement\cod.exe 2025.01.26 - 08.49.14.01.mp4");
+            mediaElement.Source = enderecoVideo;
+            mediaElement.LoadedBehavior = MediaState.Manual;
+            mediaElement.Pause();
+            mediaElement.Play();
+           
+        }
+    }
+}
