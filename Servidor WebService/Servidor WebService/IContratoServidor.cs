6a8fff805424144e6cb7e258830cc1db24672337
Marcel Henrique Gonçalves <marcel.henrique1989@gmail.com>
2025-02-11 05:00:44 -0300

    Aula Servidor WebService
    É ensinado a como criar um Servidor local

---
+++ Servidor WebService/Servidor WebService/IContratoServidor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace Servidor_WebService
+{
+    [ServiceContract]
+    internal interface IContratoServidor
+    {
+        [OperationContract]
+        bool EnviaStringProServidor(string pMensagemEnviada);
+        [OperationContract]
+        string PegaStringDoServidor();
+    }
+}
---
+++ Servidor WebService/Servidor WebService/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servidor_WebService
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MeuServidorWebService meuServidorLocal = new MeuServidorWebService("http://localhost:8080/MeuServidorLocal");
+            Console.WriteLine("Pressione qualquer tecla para sair");
+            Console.ReadKey();
+        }
+    }
+}
